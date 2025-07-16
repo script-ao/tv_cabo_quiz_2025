39c7a54d5b8df488169ed90e2d0dc3e23fc57495
tchiinhemba <tchiinhemba@gmail.com>
2025-07-16 16:10:20 +0100

    Closes #8 | Certificate screen created

---
+++ src/screens/certificate/certificate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Certificate() {
+  return (
+    <React.Fragment>
+      <section>
+        <main>
+          <h1>Certificate Screen</h1>
+        </main>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export { Certificate };
