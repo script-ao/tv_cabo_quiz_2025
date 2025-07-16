cbb118467d7f3feb9f9b1174f12ec91a73ad2df4
yuransimao <yuransimao7@gmail.com>
2025-07-16 23:29:00 +0100

    register screen

--- src/screens/register/register.jsx
+++ src/screens/register/register.jsx
@@ -3,9 +3,31 @@ import React from "react";
 function Register() {
   return (
     <React.Fragment>
-      <section>
-        <main>
-          <h1>Register Screen</h1>
+      <section className="re_wrapper">
+        <main className="re_container">
+          <div className="re_highlight">
+            <h2>PREENCHE <br/>O FORMULÁRIO </h2>
+          </div>
+
+            <form className="re_form">
+          <div className="re_container_form">
+              <div className="re_container_input">
+                <label htmlFor="name_input">Nome: <span>*</span></label>
+                <input type="text" id="name_input"/>
+              </div>
+              <div className="re_container_input">
+                <label htmlFor="email_input">Email: <span>*</span></label>
+                <input type="email" id="email_input"/>
+              </div>
+              <div className="re_container_input">
+                <label htmlFor="phone_input">Email: <span>*</span></label>
+                <input type="number" id="phone_input"/>
+              </div>
+          </div>
+            <div className="re_btn">
+              <button>Enviar</button>
+            </div>
+            </form>
         </main>
       </section>
     </React.Fragment>
