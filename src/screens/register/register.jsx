eb321a476f162a6b6f9783b6ddbaa561babe3528
tchiinhemba <tchiinhemba@gmail.com>
2025-07-16 16:11:28 +0100

    Closes #7 | Register screen created

---
+++ src/screens/register/register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Register() {
+  return (
+    <React.Fragment>
+      <section>
+        <main>
+          <h1>Register Screen</h1>
+        </main>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export { Register };
