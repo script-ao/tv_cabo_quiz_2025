ebf1cfe17a3ba80cea3f4adc251a4bca7df9d00a
yuransimao <yuransimao7@gmail.com>
2025-07-16 22:28:04 +0100

    trivia screen

--- src/screens/trivia/trivia.jsx
+++ src/screens/trivia/trivia.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import comanndo from "../../assets/images/svgs/icon/comando.svg"
+import world from "../../assets/images/svgs/icon/mundo.svg"
 function Trivia() {
 
-  const respost= ["Internet Ilimitada", "Plano de tv", "Plano voz", "TvCabo Play"]
+  const respost = ["Internet Ilimitada", "Plano de tv", "Plano voz", "TvCabo Play"]
   return (
     <React.Fragment>
       <section className="tr_wrapper">
         <main className="tr_container">
           <div className="tr_progress-bar">
-  <div className="tr_progress-fill"></div>
-</div>
+            <div className="tr_progress-fill"></div>
+          </div>
           <div className="tr_container_aw">
-          <div className="tr_container_questions">
-            <div>
-              <div>
-                
+            <div className="tr_container_questions">
+              <div className="tr_word">
+               
+                  <div className="tr_world_img">
+                    <img src={world} alt="" />
+                    
+                  </div>
+                 
+               
+
+                <div className="tr_separation"></div>
+              </div>
+              <div className="tr_questions">
+                <p>Qual serviço TVCABO é essencial para garantir que a sua experiência de jogo online seja fluida e sem interrupções, mesmo em momentos de pico?</p>
               </div>
             </div>
-            <div className="tr_questions">
-              <p>Qual serviço TVCABO é essencial para garantir que a sua experiência de jogo online seja fluida e sem interrupções, mesmo em momentos de pico?</p>
-            </div>
-          </div>
 
-          <div className="tr_resp">
+            <div className="tr_resp">
               {respost.map((item, index) => <button className="tr_btn_resp" key={index}>{item}</button>)}
-          </div>
+            </div>
           </div>
         </main>
       </section>
